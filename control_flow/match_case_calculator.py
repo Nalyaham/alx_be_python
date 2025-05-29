cce25d88e48d18e92134e3f1c79451077d3ea5c9
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-05-29 14:59:04 +0300

    Added code to the file match_case_calculator.py

---
+++ control_flow/match_case_calculator.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+match operation:
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        result = num1 / num2
+    case _:
+        print("Inavlid operator entered.")
+
+print("The result is ", result)
