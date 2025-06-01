de8a3e22a509c790cf0f4bbe36cd0baa4b4b8a44
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-06-02 00:06:11 +0300

    Adjusted the code and maintained

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -7,7 +7,7 @@ match priority:
         if time == "yes":
             print("Reminder: '", task, "' is a high prioirity task that requires immediate attention today!")
         if time == "no":
-            print("Reminder '", task, "'  a high priority task. Consider completing it when you have free time.")
+            print("Reminder: '", task, "'  a high priority task. Consider completing it when you have free time.")
     case "low":
         if time == "yes":
             print("Note: '", task, "' is a low prioirity task that requires immediate attention today!")
