d4d249968ba32f5e2a273a232a6e0597f30f8840
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-06-05 14:48:05 +0300

    Added the file named arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,25 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                return "error"
+            else:
+                return num1 / num2
+def main():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+
+    result = perform_operation(num1, num2, operation)
+    print(f"Result: {result}")
+
+if __name__ == "__main__":
+    main()
+        
