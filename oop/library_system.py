ce0b9da8587c6a428400eb0c17bec09c40314840
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-06-23 09:24:14 +0300

    library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -24,8 +24,8 @@ class PrintBook(Book):
 
 
 class Library:
-    def __init__(self, books=None):
-        self.books = books if books is not None else []
+    def __init__(self):
+        self.books = []
         
     def add_book(self, book):
         self.books.append(book)
